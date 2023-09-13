810aba73a0127a027162210b9194b6c20903f0a0
trevor-scofield <20018573+trevor-scofield@users.noreply.github.com>
2023-09-13 19:15:55 +0000

    Here is the Start of problem one but it is still a work in progress

--- make-multi-filter.js
+++ make-multi-filter.js
@@ -1,3 +1,13 @@
 //here is the multifiler JavaScript file
-const MakeMultifilter;
+function MakeMultiFilter(originalArray){
+    const currentArray = originalArray;
 
+    function arrayFilterer(elem){
+        const tempArray = new Array;
+        for(i == 0; i < originalArray.length(); i++){
+           if(originalArray[i] != elem){
+                tempArray.push(originalArray[i]);
+           }     
+        }elseIf(function)
+    }
+}
